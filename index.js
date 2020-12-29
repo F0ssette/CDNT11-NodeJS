21cf7844c3e5fb4b9887f2ee8a345f6f2ea46e22
LucieKZK <lucie.kozik@social.aston-ecole.com>
2020-12-29 15:48:14 +0100

    TP Day 2 : CRUD with MongoDB

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const express = require('express')
+const orders = require('./orders')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
-let orders =require('./orders')
+
+// Pour lutter contre les CORS
+app.use(bodyParser.urlencoded({ encoded: true }))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
+
+// Connection à MongoDB
+mongoose.connect('mongodb+srv://BruceWayne:iambatman@cluster0.xebr2.mongodb.net/orders?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 // Affichage de la page d'accueil 
 app.get('/', (req, res) => {
   res.send('I am Batman.')
 })
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
-
 app.use('/orders', orders)
 
 // Affichage du message d'erreur pour toutes les autres routes
 app.use('*', (req, res) => {
-  res.send('Error 404, page not found')
+  res.status(404).send('Error 404, page not found.')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
 })
--- orders.js
+++ orders.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var router = express.Router()
-const { v4: uuidv4, v4 } = require('uuid');
+const express = require('express')
+const router = express.Router()
+const { v4: uuidv4 } = require('uuid');
+const Order = require('./models/order')
 
 // Log de la date
 router.use(function timeLog (req, res, next) {
@@ -16,31 +17,57 @@ router.use(function theWay (req, res, next) {
 
 // CRUD get de la liste des orders
 router.get('/', function(req, res) {
-    res.status(200).send('Voici la liste des orders')
+    // res.status(200).send('Voici la liste des orders :')
+    Order.find()
+    .then(orders => res.status(200).json(orders))
+    .catch(error => res.status(400).json(error))
 })
 
 // CRUD get/:id pour voir un order en particulier
 router.get('/:id', function(req, res) {
     let id = req.params.id
-    res.status(200).send('Order id : ' + id)
+    // res.status(200).send('Order id : ' + id)
+    Order.findById(id)
+    .then(orders => res.status(200).json(orders))
+    .catch(error => res.status(400).json(error))
 })
 
 // CRUD post avec la création de l'order
 router.post('/', function(req, res) {
-    const id = uuidv4()
-    res.status(201).send('Order with id : ' + id + ' created')
+    // const id = uuidv4()
+    // res.status(201).send('Order with id : ' + id + ' created')
+    Order.create({
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        userId: req.body.userId,
+        price: req.body.price
+    })
+    .then(orders => res.status(201).json(orders))
+    .catch(error => res.status(400).json(error))
 })
 
 // CRUD update pour mettre à jour l'order selectionné par l'id
+//orders => res.status(201).json(orders)
 router.put('/:id', function(req, res) {
     const id = req.params.id
-    res.status(200).send('Order with id : ' + id + ' updated')
+    // res.status(200).send('Order with id : ' + id + ' updated')
+    Order.updateOne({_id : id}, {
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        userId: req.body.userId,
+        price: req.body.price
+    })
+    .then(orders => res.status(201).json(id + ' updated'))
+    .catch(error => res.status(400).json(error))
 })
 
 // CRUD delete pour supprimer l'order selectionné par l'id
 router.delete('/:id', function(req, res) {
     let id = req.params.id
-    res.status(204).send()
+    // res.status(204).send()
+    Order.deleteOne({_id : id})
+    .then(orders => res.status(204))
+    .catch(error => res.status(400).json(error))
 })
 
 module.exports = router
