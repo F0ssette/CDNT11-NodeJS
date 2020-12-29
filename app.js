8efcc7ed16244979ca3fc01545425a5cfc5e9394
LucieKZK <lucie.kozik@social.aston-ecole.com>
2020-12-29 13:37:49 +0100

    correction du TP

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+// Correction du TP 
 const express = require('express')
 const app = express()
 const port = 3000
-let orders =require('./orders')
+const orders =require('./orders')
 
-// Affichage de la page d'accueil 
 app.get('/', (req, res) => {
-  res.send('I am Batman.')
+    res.send('Hello World !')
 })
 
 app.listen(port, () => {
@@ -14,7 +14,6 @@ app.listen(port, () => {
 
 app.use('/orders', orders)
 
-// Affichage du message d'erreur pour toutes les autres routes
 app.use('*', (req, res) => {
-  res.send('Error 404, page not found')
-})
+    res.status(404).send('Error 404, page not found.')
+  })
--- orders.js
+++ orders.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var router = express.Router()
-const app = express()
 const { v4: uuidv4, v4 } = require('uuid');
 
 // Log de la date
@@ -9,39 +8,39 @@ router.use(function timeLog (req, res, next) {
     next()
 })
 
-// log du chemin
+// log de l'url
 router.use(function theWay (req, res, next) {
-    console.log(req.path)
+    console.log('Method : ' + req.method + ' on Endpoint : '+ req.baseUrl + req.url)
     next()
 })
 
 // CRUD get de la liste des orders
 router.get('/', function(req, res) {
-    res.send('Voici la liste des orders')
+    res.status(200).send('Voici la liste des orders')
 })
 
 // CRUD get/:id pour voir un order en particulier
 router.get('/:id', function(req, res) {
     let id = req.params.id
-    console.log('Voici l order d ID : ', id)
-    res.send(id)
+    res.status(200).send('Order id : ' + id)
 })
 
 // CRUD post avec la création de l'order
-router.post('/new', function(req, res) {
-    res.send(uuidv4())
+router.post('/', function(req, res) {
+    const id = uuidv4()
+    res.status(201).send('Order with id : ' + id + ' created')
 })
 
 // CRUD update pour mettre à jour l'order selectionné par l'id
-router.put('/edit/:id', function(req, res) {
-    res.send(req.params.id)
+router.put('/:id', function(req, res) {
+    const id = req.params.id
+    res.status(200).send('Order with id : ' + id + ' updated')
 })
 
 // CRUD delete pour supprimer l'order selectionné par l'id
-router.delete('/delete/:id', function(req, res) {
+router.delete('/:id', function(req, res) {
     let id = req.params.id
-    console.log(id)
-    res.send('L order a bien été supprimé')
+    res.status(204).send()
 })
 
 module.exports = router
